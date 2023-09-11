3aec0a14de301b742cf361e02d9eafb05328e3a6
Gilsabo <gilsala_88@hotmail.com>
2023-09-11 20:07:23 +0200

    Add options for hue luminosity and size

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { useState } from 'react';
 import { Button, Header, Main, TextColor } from './AppStyled';
 
 export default function App() {
+  const [hue, setHue] = useState('');
+  const [luminosity, setLuminosity] = useState('');
   const [randomColorGenerator, setRandomColorGenerator] = useState('');
-
+  const [size, setSize] = useState('');
   return (
     <Main>
       <Header>React random color generator</Header>
       <div
         style={{
           backgroundColor: randomColorGenerator,
-          width: '300px',
-          height: '300px',
+          width: size === '' ? '300px' : `${size}px`,
+          height: size === '' ? '300px' : `${size}px`,
           display: 'flex',
           justifyContent: 'center',
           borderRadius: '5px',
@@ -30,6 +32,35 @@ export default function App() {
       >
         Generate
       </Button>
+      <div>
+        <input
+          onChange={(event) => {
+            const value = event.currentTarget.value;
+            setHue(value);
+            setRandomColorGenerator(
+              randomColor({ luminosity: 'light', hue: hue }),
+            );
+          }}
+        />
+        <div>{randomColorGenerator}1</div>
+        <input
+          onChange={(event) => {
+            const value = event.currentTarget.value;
+            setLuminosity(value);
+            setRandomColorGenerator(
+              randomColor({ luminosity: luminosity, hue: hue }),
+            );
+          }}
+        />
+        <div>{randomColorGenerator}2</div>
+        <input
+          onChange={(event) => {
+            const value = event.currentTarget.value;
+            setSize(value);
+          }}
+        />
+        <div>{size}3</div>
+      </div>
     </Main>
   );
 }
