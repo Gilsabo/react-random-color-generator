45a325198d812bc5e421d7699e549049825649f4
Gilsabo <gilsala_88@hotmail.com>
2023-09-11 16:10:40 +0200

    Add button with random color generator

--- src/App.js
+++ src/App.js
@@ -1,3 +1,20 @@
+import randomColor from 'randomcolor';
+import { useState } from 'react';
+
 export default function App() {
-  return <div className="App">Hello World</div>;
+  const [randomColorGenerator, setRandomColorGenerator] = useState('');
+  return (
+    <div className="App">
+      <header>React random color generator</header>
+      <div style={{ backgroundColor: randomColorGenerator }}>
+        Generated Color: {randomColorGenerator}
+      </div>
+      <button
+        type="button"
+        onClick={() => setRandomColorGenerator(randomColor())}
+      >
+        Generate
+      </button>
+    </div>
+  );
 }
